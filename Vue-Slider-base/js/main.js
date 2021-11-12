1d86527c49cd33183fa581ab44c4da0b18dee551
artemisia000 <misiapierobon@gmail.com>
2021-11-12 15:48:41 +0100

    prev e next thumbnails

--- Vue-Slider-base/js/main.js
+++ Vue-Slider-base/js/main.js
@@ -9,27 +9,27 @@ const app = new Vue ({
         {
         image: 'img/01.jpg',
         title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',    
         },
         {
         image: 'img/02.jpg',
-        title: 'Francia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        title: 'Svizzera',
+        text: 'Lorem ipsum.' ,   
         },
         {
         image: 'img/03.jpg',
-        title: 'Italia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',    
         },
         {
         image: 'img/04.jpg',
         title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',    
         },
         {
         image: 'img/05.jpg',
-        title: 'Inghilterra',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        title: 'Palma',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.' ,   
         },
 
 ],
@@ -37,4 +37,37 @@ const app = new Vue ({
 activeSlide:0,
 
 },
+
+
+//METHODS//
+
+
+methods: {
+
+    //prev
+
+    prev() {
+
+        this.activeSlide--;
+
+        if (this.activeSlide < 0) {
+            this.activeSlide = this.slides.length - 1;
+        }
+    },
+
+    //next
+
+    next() {
+
+        this.activeSlide++;
+        
+
+        if(this.activeSlide > 0) {
+            this.activeSlide = this.activeSlide.length - 1;
+            this.activeSlide = 0;
+        }
+    }
+
+}
+
 });
