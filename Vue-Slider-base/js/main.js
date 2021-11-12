88f57f6a81ab922d957a1f8e55e327b131be6473
artemisia000 <misiapierobon@gmail.com>
2021-11-12 15:00:47 +0100

    data e oggetti

---
+++ Vue-Slider-base/js/main.js
@@ -0,0 +1,40 @@
+/*
+ * Vue Slider
+ */
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        slides: [
+        {
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        },
+        {
+        image: 'img/02.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        },
+        {
+        image: 'img/03.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        },
+        {
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        },
+        {
+        image: 'img/05.jpg',
+        title: 'Inghilterra',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'    
+        },
+
+],
+
+activeSlide:0,
+
+},
+});
